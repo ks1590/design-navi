68080f141f368e9bc8a8c9202f75771f4c2776b7
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-02-24 11:30:29 +0900

    add: ナビゲーションリンクとサンプルページ追加

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+	return (
+		<div>
+			<h1>About Us</h1>
+			<p>
+				This is the About Page. Here you can learn more about us and our
+				mission.
+			</p>
+		</div>
+	);
+}
+
+export default About;
